257355f193d29ffeba20db506b5a743fda56b8c6
vierifath <vieri.ayuba@gmail.com>
2021-06-08 03:08:49 +0700

    add file

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="<?= base_url('css/profile.css') ?>" />
+
+	<title>Vieri Fath</title>
+</head>
+
+<body></body>
+
+<header class="l-header">
+	<nav class="nav bd-grid">
+		<div>
+			<a href="index.html" class="nav__logo">Vieri Fath</a>
+		</div>
+
+		<div class="nav__menu" id="nav-menu">
+			<ul class="nav__list">
+				<!-- <li class="nav__item"><a href="index.html" class="nav__link active">Homepage</a></li> -->
+			</ul>
+		</div>
+
+		<div class="nav__toggle" id="nav-toggle">
+			<i class='bx bx-menu'></i>
+		</div>
+	</nav>
+</header>
+
+<main class="l-main">
+	<!--===== HOME =====-->
+	<section class="home bd-grid" id="home">
+		<div class="home__data">
+			<h1 class="home__title"><span class="home__title-color">Vieri Fath Ayuba</span><br> <span class="home__ITStudent">IT Student</span></h1>
+			<div class="identity">
+				<p>Full Name : Vieri Fath Ayuba</p>
+				<p>Student ID : 05111840000153</p>
+				<p>Address: Merpati Street No. 61 Lamongan</p>
+				<p>Date of Birth: October 30th, 1999</p>
+				<p>Nasionality: Indonesian</p>
+				<p>Mother Toungue: Indonesian</p>
+			</div>
+		</div>
+		<div class="home__img">
+			<img src="img/fotoprofil.jpg" alt="Foto Vieri">
+		</div>
+	</section>
+
+
+	<!--===== FOOTER =====-->
+	<footer class="footer">
+		<p class="footer__title">Vieri Fath</p>
+		<p class="ftr">Assignment #6 - Pemrograman Berbasis Kerangka Kerja (B)</p>
+	</footer>
+
+	<!--===== SCROLL REVEAL =====-->
+	<!-- <script src="scrollreveal.js"></script> -->
+
+	<!-- Script JS -->
+	<!-- <script src="main.js"></script> -->
+	</body>
+
+</html>
